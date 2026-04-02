336137c5b54adc0bb6371fdde1e49c9dfbc639a7
Hapid Fadli <hapidzfadli@gmail.com>
2026-04-02 14:19:25 +0700

    contact

--- src/components/sections/contact-hero-section.tsx
+++ src/components/sections/contact-hero-section.tsx
@@ -5,8 +5,14 @@ export function ContactHeroSection() {
     <PageHero
       src="/images/contact/hero-bg.jpg"
       alt="Contact Us"
-      title="Contact Us"
-      imageClassName="scale-150 translate-x-[8%] object-[center_15%]"
+      title={
+        <span className="max-w-[275px] text-[42px] md:max-w-none md:text-[64px]">
+          Contact Us
+        </span>
+      }
+      className="h-[360px] md:h-100"
+      imageClassName="scale-150 translate-x-[8%] object-[center_15%] md:scale-150"
+      showBottomGradient={false}
     />
   );
 }
